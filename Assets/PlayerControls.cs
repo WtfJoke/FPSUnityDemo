71d9c9cfca3022129bb3e87bc9a5a166207697e7
Manuel <wtfjoke@livenet.ch>
2018-04-22 14:17:06 +0200

    add proof of concept of building ground

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControls : MonoBehaviour {
+public class PlayerControls : MonoBehaviour
+{
 
     public Camera cam;
     public LayerMask interactionLayer;
@@ -16,15 +17,20 @@ public class PlayerControls : MonoBehaviour {
     public GameObject bombPrefab;
     public GameObject wallPrefab;
     public WallConnection previewWall;
+    public WallConnection previewGround;
+    private GameObject selectedBuildObject;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        selectedBuildObject = previewWall.gameObject;
+        previewGround.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.T))
         {
             if (Time.timeScale == 1)
@@ -36,7 +42,7 @@ public class PlayerControls : MonoBehaviour {
                 Time.timeScale = 1;
             }
         }
-		if (Input.GetKey(KeyCode.Mouse1))
+        if (Input.GetKey(KeyCode.Mouse1))
         {
             if (objInHand != null)
             {
@@ -45,14 +51,15 @@ public class PlayerControls : MonoBehaviour {
             RaycastHit hit;
             Ray ray = new Ray(cam.transform.position, cam.transform.forward);
             Debug.DrawLine(ray.origin, ray.GetPoint(maxRange));
-            if(Physics.Raycast(ray, out hit, maxRange, interactionLayer.value))
+            if (Physics.Raycast(ray, out hit, maxRange, interactionLayer.value))
             {
                 objInHand = hit.transform.GetComponent<Rigidbody>();
                 objInHand.isKinematic = true;
                 objInHand.transform.position = handPosition.position;
                 objInHand.transform.parent = handPosition;
             }
-        }else if (Input.GetKeyDown(KeyCode.Mouse0))
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             if (objInHand == null)
             {
@@ -60,24 +67,64 @@ public class PlayerControls : MonoBehaviour {
                 GameObject bomb = GameObject.Instantiate(bombPrefab, handPosition.position, Quaternion.identity);
                 objInHand = bomb.GetComponent<Rigidbody>();
             }
-            
-            objInHand.isKinematic = false;  
+
+            objInHand.isKinematic = false;
             objInHand.transform.parent = null;
             objInHand.AddForce(cam.transform.forward * throwForce);
             objInHand = null;
-            
-
-        }else if (Input.GetKeyDown(KeyCode.B))
-        {
-            Vector3 position = GetWallPosition();
-            Vector3 rotation = GetWallRotation();
-            Quaternion rotationQuaternion = Quaternion.Euler(rotation.x, rotation.y, rotation.z);
-            GameObject placedWall = Instantiate(wallPrefab, position, rotationQuaternion);
-            placedWall.GetComponent<WallConnection>().placed = true;
-
         }
-        
-        previewWall.transform.SetPositionAndRotation(GetWallPosition(), GetWallRotationQuaternion());//Quaternion.Euler(GetNearestDegree(transform.rotation.eulerAngles)));
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            previewWall.gameObject.SetActive(true);
+            selectedBuildObject = previewWall.gameObject;
+            previewGround.gameObject.SetActive(false);
+        }else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            previewGround.gameObject.SetActive(true);
+            selectedBuildObject = previewGround.gameObject;
+            previewWall.gameObject.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (ReferenceEquals(selectedBuildObject, previewWall.gameObject))
+            {
+                Vector3 position = GetWallPosition();
+                Quaternion rotation = GetWallRotationQuaternion();
+                GameObject placedWall = Instantiate(wallPrefab, position, rotation);
+                placedWall.GetComponent<WallConnection>().placed = true;
+            }
+            else
+            {
+                Vector3 position = GetGroundPosition();
+                
+                GameObject placedGround = Instantiate(previewGround.wallObj.gameObject, position, Quaternion.identity);
+                placedGround.GetComponent<WallConnection>().placed = true;
+            }
+        }
+
+
+        selectedBuildObject.transform.SetPositionAndRotation(GetWallPosition(), GetWallRotationQuaternion());
+
+
+    }
+
+    private Vector3 GetPosition()
+    {
+        if (ReferenceEquals(selectedBuildObject, previewWall.gameObject))
+        {
+            return GetWallPosition();
+        }
+        else
+        {
+            return GetGroundPosition();
+        }
+    }
+
+    private Vector3 GetGroundPosition()
+    {
+        Vector3 gridPosition = GetNearestPointOnGrid(spawnPosition.position, 1f);
+        Vector3 position = new Vector3(gridPosition.x + 0.5f, gridPosition.y + wallPrefab.transform.position.y, gridPosition.z + 0.5f);
+        return position;
     }
 
     private Vector3 GetWallRotation()
